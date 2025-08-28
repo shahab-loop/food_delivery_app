f0f200c33a47b8c3d89b77211ec096151e52c2bb
shahab-loop <roberthack782@gmail.com>
2025-08-27 18:45:03 -0700

    assets are given

---
+++ lib/core/res/theme/theme_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/animation.dart';
+
+class ThemeManager {
+  static const Color textColor1 = Color(0xff858C83);
+  static const Color textColor2 = Color(0xffFFFFFF);
+  static const Color textColor3 = Color(0xffFFFFFF);
+  static const Color ratingColor1 = Color(0xff746969);
+  static const Color searchColor1 = Color(0xff746969);
+  static const Color profileColor1 = Color(0xff746969);
+  static const Color drawerColor = Color(0xffFF785B);
+  static const Color backgroundColor = Color(0xffFF785B);
+  static const Color discountContainerColor = Color(0xffFF785B);
+
+  
+ }
